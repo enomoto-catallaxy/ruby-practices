5d66038edb7978bf961d80ef54836da438642360
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-21 18:41:43 +0900

    lsコマンド修正4/21

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,41 +1,40 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 class Sort
-    def initialize(number)
-        @number = number
-    end
+  def initialize(number)
+    @number = number
+    @array = [] * @number
+  end
 
-    def matlix(given_array)
-        array = []*@number
-        k = 0
-        while k < @a_n do
-            j = 0
-            while j < @number do
-                array[j] = given_array[k + j * @a_n]
-                printf("%15s\t", array[j])
-                j += 1
-            end
-            printf("\n")
-            k += 1
-            array = []*@number
-        end
+  def matlix(given)
+    @k = 0
+    while @k < @a_n
+      @j = 0
+      while @j < @number
+        @array[@j] = given[@k + (@j * @a_n)]
+        printf("%15s\t", @array[@j])
+        @j += 1
+      end
+      printf("\n")
+      @k += 1
+      @array = [] * @number
     end
+  end
 
-    def sort(given_array)
-        length = given_array.length
-        remainder = length / @number
-        if remainder == 0
-            @a_n = length / @number
-        else
-            @a_n = length / @number + 1
-            shortage = @number - remainder
-            shortage.times do
-                given_array = given_array.push("")
-            end
-        end
-        matlix(given_array)
+  def sort(given)
+    remainder = given.length / @number
+    if remainder.zero?
+      @a_n = given.length / @number
+    else
+      @a_n = (given.length / @number) + 1
+      (@number - remainder).times do
+        given.push(``)
+      end
     end
+    matlix(given)
+  end
 end
 
-given_array = Dir.glob('*')
+given = Dir.glob('*')
 object = Sort.new(3)
-object.sort(given_array)
+object.sort(given)
