cd0d8811edf47f5516ef50d4eb13b86422cf820e
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-22 17:17:42 +0900

    fizzbuzz修正#22/4/22

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(number)
-  number.times do |i|
-    if  i % 3 == 0
+  (number + 1).times do |i|
+    if i.zero?
+      puts ""
+    elsif  i % 15 == 0
+      puts "FizzBuzz"
+    elsif i %3 == 0
       puts "Fizz"
     elsif i % 5 == 0
       puts "Buzz"
