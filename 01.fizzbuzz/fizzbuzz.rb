62f3b60d93d7702ce83e6d8087d9386603385d6d
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-25 17:08:05 +0900

    fizzbuzzプログラムの修正#22/4/25

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,27 +1,16 @@
-def main
-  object = Number.new(20)
-  object.fizzbuzz
-end
-
-class Number
-  def initialize(number)
-    @number = number
-  end
-
-  def fizzbuzz
-    numbers = (1..@number)
-    numbers.each do |i|
-      if  i % 15 == 0
-        puts "FizzBuzz"
-      elsif i %3 == 0
-        puts "Fizz"
-      elsif i % 5 == 0
-        puts "Buzz"
-      else
-        puts i
-      end
+def fizzbuzz(number)
+  numbers = (1..number)
+  numbers.each do |i|
+    if  i % 15 == 0
+      puts "FizzBuzz"
+    elsif i %3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
     end
   end
 end
 
-main
+fizzbuzz(20)
