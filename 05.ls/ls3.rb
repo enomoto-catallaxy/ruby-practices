277e9a523415152c037bdb8e0929b2191530b7fb
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-29 00:25:48 +0900

    ls3コマンド修正4/29

--- 05.ls/ls3.rb
+++ 05.ls/ls3.rb
@@ -4,43 +4,51 @@ require 'optparse'
 
 def main
   given = Dir.glob('*')
+  @object = LsCommand.new(3)
+  given = option_r(given)
+  @object.run(given)
+end
+
+def option_r(given)
   opt = OptionParser.new
   opt.on('-r') { given = Dir.glob('*').reverse }
   opt.parse(ARGV)
-  object = LsCommand.new(3)
-  object.divide_case(given)
+  given
 end
 
 class LsCommand
   def initialize(number)
     @number = number
-    @array = []
   end
 
   def print_column(given)
+    array = []
     @layer.times do |vertical|
       @number.times do |horizontal|
-        @array[horizontal] = given[vertical + (horizontal * @layer)]
-        printf("%15s\t", @array[horizontal])
-        horizontal += 1
+        array[horizontal] = given[vertical + (horizontal * @layer)]
+        printf("%30s\t", array[horizontal])
       end
       puts("\n")
-      vertical + 1
-      @array = []
+      array = []
     end
   end
 
-  def divide_case(given)
-    remainder = given.length / @number
-    if remainder.zero?
+  def divide_array(given)
+    quotient = given.length / @number
+    if quotient.zero?
       @layer = given.length / @number
     else
       @layer = (given.length / @number) + 1
-      (@number - remainder).times do
+      (@number - quotient).times do
         given.push('')
       end
     end
-    print_column(given)
+    given
+  end
+
+  def run(given)
+    devided_array = divide_array(given)
+    print_column(devided_array)
   end
 end
 
