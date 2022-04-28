b9960d08285921b847038b0e7420856e2510cba0
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-28 17:11:33 +0900

    lsコマンド修正4/28

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -3,36 +3,39 @@
 class LsCommand
   def initialize(number)
     @number = number
-    @array = []
   end
 
   def print_column(given)
+    array = []
     @layer.times do |vertical|
       @number.times do |horizontal|
-        @array[horizontal] = given[vertical + (horizontal * @layer)]
-        printf("%15s\t", @array[horizontal])
-        horizontal + 1
+        array[horizontal] = given[vertical + (horizontal * @layer)]
+        printf("%15s\t", array[horizontal])
       end
       puts("\n")
-      vertical += 1
-      @array = []
+      array = []
     end
   end
 
-  def divide_case(given)
-    remainder = given.length / @number
-    if remainder.zero?
+  def divide_array(given)
+    quotient = given.length / @number
+    if quotient.zero?
       @layer = given.length / @number
     else
       @layer = (given.length / @number) + 1
-      (@number - remainder).times do
+      (@number - quotient).times do
         given.push('')
       end
     end
-    print_column(given)
+    given
+  end
+
+  def run(given)
+    devided_array = divide_array(given)
+    print_column(devided_array)
   end
 end
 
 given = Dir.glob('*')
 object = LsCommand.new(3)
-object.divide_case(given)
+object.run(given)
