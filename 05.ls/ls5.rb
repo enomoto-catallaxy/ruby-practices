dab08126e3ab33cfbfa4994b0b2983b92962e57f
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-01 01:46:47 +0900

    ls5コマンド実装

--- 05.ls/ls5.rb
+++ 05.ls/ls5.rb
@@ -6,8 +6,7 @@ require 'etc'
 def main
   given = Dir.glob('*')
   @object = LsCommand.new(3)
-  option = Options.new
-  given = option.multiple_options(given)
+  given = multiple_options(given)
   @object.run(given)
 end
 
@@ -47,35 +46,39 @@ class LsCommand
   end
 end
 
-class Options
-  def initialize
-    @times = []
-    @sizes = []
-    @nlinks = []
-    @permissions = []
-    path = Dir.pwd
-    @user = Etc.getpwuid(File.stat(path).uid).name
-    @group = Etc.getgrgid(File.stat(path).gid).name
-  end
+def arguments
+  @times = []
+  @sizes = []
+  @nlinks = []
+  @permissions = []
+  path = Dir.pwd
+  @user = Etc.getpwuid(File.stat(path).uid).name
+  @group = Etc.getgrgid(File.stat(path).gid).name
+end
 
-  def multiple_options(given)
-    opt = OptionParser.new
-    opt.on('-a') { given = Dir.glob('*', File::FNM_DOTMATCH) }
-    opt.on('-r'){ given = Dir.glob('*').reverse }
-    opt.on('-l') do
-      puts "total #{given.length}"
-      @object = LsCommand.new(1)
-      given.length.times do |i|
-        @times[i] = "%30s\t" % File.atime(given[i]).to_s
-        @sizes[i] = "%10s\t" % File.size(given[i]).to_s
-        @nlinks[i] = "%5s\t" % File::Stat.new(given[i]).nlink.to_s
-        @permissions[i] ="0%o" % File.stat(given[i]).mode
-        given[i] = @permissions[i] + @nlinks[i] + @user + @group+ @sizes[i]+ @times[i]+ given[i] 
-      end
-    end
-    opt.parse(ARGV)
-    given
+def multiple_options(given)
+  arguments
+  opt = OptionParser.new
+  options = ARGV.getopts("a", "r", "l")
+  if options["a"]
+    given = Dir.glob('*', File::FNM_DOTMATCH)
   end
+  if options["r"]
+    given = given.reverse
+  end
+  if options["l"]
+    puts "total #{given.length}"
+    @object = LsCommand.new(1)
+    given.length.times do |i|
+      @times[i] = "%30s\t" % File.atime(given[i]).to_s
+      @sizes[i] = "%10s\t" % File.size(given[i]).to_s
+      @nlinks[i] = "%5s\t" % File::Stat.new(given[i]).nlink.to_s
+      @permissions[i] ="0%o" % File.stat(given[i]).mode
+      given[i] = @permissions[i] + @nlinks[i] + @user + @group+ @sizes[i]+ @times[i]+ given[i] 
+    end
+  end
+  
+  given
 end
 
 main
