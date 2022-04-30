8cd988555425e3f7dd518c58adda6e9f99df628b
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-04-30 17:52:35 +0900

    ボーリングスコアプログラムの修正4/30

--- 04.bowling/bowl.rb
+++ 04.bowling/bowl.rb
@@ -1,9 +1,9 @@
-input_array = gets.split(",")
+# frozen_string_literal: true
+
+input_array = gets.split(',')
 
 input_array.length.times do |i|
-  if input_array[i] == "X"
-    input_array[i] = 10
-  end
+  input_array[i] = 10 if input_array[i] == 'X'
   input_array[i] = input_array[i].to_i
 end
 
@@ -13,18 +13,18 @@ count = 0
 score = [0] * max_frame
 
 input_array.length.times do |i|
-  if frame + 1 != max_frame #during frame1 between frame9
+  if frame + 1 != max_frame
     if input_array[i] == 10
-      if count.zero? # strike
+      if count.zero?
         count = 1
-        score[frame] = score[frame] + input_array[i] + input_array[i+1] + input_array[i+2]
-      else # 0 - 10 spare
-        score[frame] = score[frame] + input_array[i] + input_array[i+1] 
+        score[frame] = score[frame] + input_array[i] + input_array[i + 1] + input_array[i + 2]
+      else
+        score[frame] = score[frame] + input_array[i] + input_array[i + 1]
       end
-    elsif input_array[i] + score[frame] == 10 # other spare patterns
-      score[frame] = score[frame] + input_array[i] + input_array[i+1]
+    elsif input_array[i] + score[frame] == 10
+      score[frame] = score[frame] + input_array[i] + input_array[i + 1]
     else
-      score[frame] = score[frame] + input_array[i] 
+      score[frame] = score[frame] + input_array[i]
     end
 
     count += 1
@@ -34,7 +34,7 @@ input_array.length.times do |i|
       frame += 1
     end
 
-  else #at frame 10
+  else
     score[frame] = score[frame] + input_array[i]
   end
 end
