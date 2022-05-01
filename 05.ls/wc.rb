67baac260958c7197c1ba5722ab8aa5dd3c889b3
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-02 01:00:57 +0900

    wcコマンド作成途中5/2

---
+++ 05.ls/wc.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require 'etc'
+
+def main
+  object = WcCommand.new
+  object.output
+end
+
+class WcCommand
+  def initialize
+    @str = []
+    @array = [3]
+  end
+
+  def output
+    ARGV.length.times do |i| #入力されたファイルの数だけ単語数などを返す
+      @str[i] = File.read(ARGV[i])
+      @array[0] = count_lines(@str[i])
+      @array[1] = count_words(@str[i])
+      @array[2] = count_characters(@str[i])
+      @array.length.times do |j|
+        printf("%4s\t" % @array[j])
+      end
+      puts('') #改行
+      @str = [3] #初期化
+    end
+  end
+  def count_lines(str)
+    str.lines.count
+  end
+  def count_words(str)
+    str.split(/\s+/).size
+  end
+  def count_characters(str)
+    str.split(/\s+/).join.chars.size
+  end
+
+end
+
+main
