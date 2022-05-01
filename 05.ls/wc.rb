a7df1cb8ced4a6be4e2a577f458e177cc53918eb
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-02 01:27:44 +0900

    wcコマンド合計を追加5/2

--- 05.ls/wc.rb
+++ 05.ls/wc.rb
@@ -4,17 +4,19 @@ require 'optparse'
 require 'etc'
 
 def main
-  object = WcCommand.new
-  object.output
+  wc_object = WcCommand.new
+  wc_object.output
 end
 
 class WcCommand
   def initialize
     @str = []
     @array = [3]
+    @sum = [0, 0, 0]
   end
 
   def output
+    count = 0
     ARGV.length.times do |i| #入力されたファイルの数だけ単語数などを返す
       @str[i] = File.read(ARGV[i])
       @array[0] = count_lines(@str[i])
@@ -22,22 +24,31 @@ class WcCommand
       @array[2] = count_characters(@str[i])
       @array.length.times do |j|
         printf("%4s\t" % @array[j])
+        @sum[j] += @array[j]
       end
-      printf("%8s\t" % ARGV[i])
-      puts('') #改行
+      printf("%8s\t\n" % ARGV[i])
       @str = [3] #初期化
+      count += 1
+      if count == ARGV.length
+        @sum.length.times do |k|
+          printf("%4s\t" % @sum[k])
+        end
+        printf("%8s\t\n" % "合計")
+      end
     end
   end
+
   def count_lines(str)
     str.lines.count
   end
+
   def count_words(str)
     str.split(/\s+/).size
   end
+
   def count_characters(str)
     str.split(/\s+/).join.chars.size
   end
-
 end
 
 main
