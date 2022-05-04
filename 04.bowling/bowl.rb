b95507a4f325e114eca60b8bc8e5d170a653c72f
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-04 16:51:21 +0900

    ボーリングスコアプログラムの修正5/4

--- 04.bowling/bowl.rb
+++ 04.bowling/bowl.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
-input_array = gets.split(',')
+input_array = ARGV[0].split(',')
 
-input_array.length.times do |i|
-  input_array[i] = 10 if input_array[i] == 'X'
-  input_array[i] = input_array[i].to_i
+input_array = input_array.map do |input|
+  input == 'X' ? 10 : input.to_i
 end
 
 frame = 0
-max_frame = 10
+MAX_FRAME = 10
 count = 0
-score = [0] * max_frame
+score = Array.new(MAX_FRAME, 0)
 
 input_array.length.times do |i|
-  if frame + 1 != max_frame
+  if frame + 1 != MAX_FRAME
     if input_array[i] == 10
       if count.zero?
         count = 1
-        score[frame] = score[frame] + input_array[i] + input_array[i + 1] + input_array[i + 2]
+        score[frame] += input_array[i] + input_array[i + 1] + input_array[i + 2]
       else
-        score[frame] = score[frame] + input_array[i] + input_array[i + 1]
+        score[frame] += input_array[i] + input_array[i + 1]
       end
     elsif input_array[i] + score[frame] == 10
-      score[frame] = score[frame] + input_array[i] + input_array[i + 1]
+      score[frame] += input_array[i] + input_array[i + 1]
     else
-      score[frame] = score[frame] + input_array[i]
+      score[frame] += input_array[i]
     end
 
     count += 1
@@ -35,7 +34,7 @@ input_array.length.times do |i|
     end
 
   else
-    score[frame] = score[frame] + input_array[i]
+    score[frame] += input_array[i]
   end
 end
 puts score.sum
