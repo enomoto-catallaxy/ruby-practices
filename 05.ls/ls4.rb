ed0d38fb438599b45cfdad2167bc8df7f27022e0
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-18 21:28:55 +0900

    ls4.rb修正

--- 05.ls/ls4.rb
+++ 05.ls/ls4.rb
@@ -4,10 +4,39 @@ require 'optparse'
 require 'etc'
 
 def main
-  given = Dir.glob('*')
-  @object = LsCommand.new(3)
-  given = option_l(given)
-  @object.run(given)
+  @command = LsCommand.new(3)
+  @options = ARGV.getopts('l')
+  given = include_option_l?
+  @command.run(given)
+end
+
+def files_with_long_details
+  puts "total #{Dir.glob('*').length}"
+  @command = LsCommand.new(1)
+  given = []
+  times = []
+  sizes = []
+  nlinks = []
+  permissions = []
+  path = Dir.pwd
+  user = Etc.getpwuid(File.stat(path).uid).name
+  group = Etc.getgrgid(File.stat(path).gid).name
+  Dir.glob('*').length.times do |i|
+    times[i] = format('%30s' % File.atime(Dir.glob('*')[i]).to_s)
+    sizes[i] = format('%10s' % File.size(Dir.glob('*')[i]).to_s)
+    nlinks[i] = format('%5s' % File::Stat.new(Dir.glob('*')[i]).nlink.to_s)
+    permissions[i] = format('0%o' % File.stat(Dir.glob('*')[i]).mode)
+    given[i] = format(permissions[i] << ' ' << nlinks[i] << ' ' << user << ' ' << group << ' ' << sizes[i] << ' ' << times[i] << ' ' << Dir.glob('*')[i])
+  end
+  given
+end
+
+def include_option_l?
+  if @options['l']
+    files_with_long_details
+  else
+    Dir.glob('*')
+  end
 end
 
 class LsCommand
@@ -16,26 +45,21 @@ class LsCommand
   end
 
   def print_column(given)
-    array = []
     @layer.times do |vertical|
       @number.times do |horizontal|
-        array[horizontal] = given[vertical + (horizontal * @layer)]
-        printf("%30s\t", array[horizontal])
+        printf("%15s\t", given[vertical + (horizontal * @layer)])
       end
-      puts("\n")
-      array = []
+      puts
     end
   end
 
   def divide_array(given)
-    quotient = given.length / @number
-    if quotient.zero?
+    remainder = given.length % @number
+    if remainder.zero?
       @layer = given.length / @number
     else
       @layer = (given.length / @number) + 1
-      (@number - quotient).times do
-        given.push('')
-      end
+      "#{given}#{' ' * (@number - remainder)}"
     end
     given
   end
@@ -44,37 +68,6 @@ class LsCommand
     devided_array = divide_array(given)
     print_column(devided_array)
   end
-end
-
-def option_l(given)
-  opt = OptionParser.new
-  opt.on('-l [VAL]') do |v|
-    @object = LsCommand.new(1)
-    times = []
-    sizes = []
-    nlinks = []
-    permissions = []
-    path = Dir.pwd
-    user = Etc.getpwuid(File.stat(path).uid).name
-    group = Etc.getgrgid(File.stat(path).gid).name
-    if v == nil
-      puts "total #{given.length}"
-    else
-      given = [] #初期化
-      (ARGV.length - 1).times do |i|
-        given[i] = ARGV[i+1]
-      end
-    end
-    given.length.times do |i|
-        times[i] = "%30s\t" % File.atime(given[i]).to_s
-        sizes[i] = "%10s\t" % File.size(given[i]).to_s
-        nlinks[i] = "%5s\t" % File::Stat.new(given[i]).nlink.to_s
-        permissions[i] ="0%o" % File.stat(given[i]).mode
-        given[i] =permissions[i]  + " " + nlinks[i] + " " +  user + " " +group+" " +sizes[i]+ " " + times[i]+ " " +  given[i] 
-      end
-  end
-  opt.parse(ARGV)
-  given
 end
 
 main
